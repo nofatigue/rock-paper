f72aee7fba9bbcac3d0fc130c8f86eece8c64787
nofatigue <nofatigua@gmail.com>
2023-12-03 20:04:15 +0200

    create playRound():
    - game.js: playRound() returns the winner of 1 round, given player + comp input.

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
+/**
+ * 
+ * @returns {string} One of ['Rock', 'Paper', 'Scissors']
+ */
 function getComputerChoice()
 {
+    /* Return one of three options: Rock, Paper, Scissors  */
+
     const randomNumber = Math.floor(Math.random() * 3);
     const choiceOptions = ['Rock', 'Paper', 'Scissors'];
     
@@ -7,4 +13,42 @@ function getComputerChoice()
 
 }
 
-console.log(getComputerChoice());
+/**
+ * 
+ * @param {string} playChoice - the choice of the player out of ['Rock', 'Paper', 'Scissors']
+ * @param {string} computerChoice - the choice of the computer, out of ['Rock', 'Paper', 'Scissors']
+ */
+function playRound(playChoice, computerChoice)
+{
+    // normalize input values
+    playChoice = playChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    WinRulesMap = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper', 
+        };
+
+    if (!WinRulesMap[playChoice])
+        return 'bad input';
+
+    
+    
+
+    if (playChoice === computerChoice)
+    {
+        return 'tie';
+    }
+
+    if (WinRulesMap[playChoice] == computerChoice)
+        return 'win';
+    else 
+        return 'lose';
+
+}
+
+let userInput = prompt("Rock, paper or scissors?");
+let computerChoice = getComputerChoice();
+
+console.log(playRound(userInput, computerChoice));
